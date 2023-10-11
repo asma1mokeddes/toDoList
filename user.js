40cf511a641f3d4ffc378b2d306142ff25b2d517
Bass913 <bastiendikiadi@outlook.fr>
2023-10-11 11:27:22 +0200

    rename property

--- user.js
+++ user.js
@@ -1,42 +1,37 @@
-class User{
+class User {
+  constructor(firstname, lastname, email, birthdate) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.birthdate = birthdate;
+  }
 
+  isValidEmail() {
+    // Expression régulière pour vérifier le format d'un email
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    // Teste si l'email correspond à l'expression régulière
+    return emailRegex.test(this.email);
+  }
 
-    constructor(firstname, familyname, email, birthdate){
-        this.firstname= firstname;
-        this.familyname = familyname;
-        this.email = email;
-        this.birthdate = birthdate;
+  isValidAge() {
+    const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateFormatRegex.test(this.birthdate)) {
+      return false;
     }
 
+    const today = new Date();
+    const birthDate = new Date(this.birthdate);
 
-    isValidEmail() {
-        // Expression régulière pour vérifier le format d'un email
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        // Teste si l'email correspond à l'expression régulière
-        return emailRegex.test(this.email);
-    }
+    return today.getFullYear() - birthDate.getFullYear() >= 13;
+  }
 
-    isValidAge(){
-        const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
-        if (!dateFormatRegex.test(this.birthdate)) {
-            return false;
-        }
+  isValidNameOrFirstName() {
+    return this.firstname !== "" && this.lastname !== "";
+  }
 
-        const today = new Date();
-        const birthDate = new Date(this.birthdate);
-
-        return today.getFullYear() - birthDate.getFullYear() >= 13;
-    }
-
-    isValidNameOrFirstName(){
-        return this.firstname !== "" && this.familyname !== "";
-    }
-
-    isValid(){
-        return this.isValidEmail() && this.isAge() && this.name();
-        
-
-    }
+  isValid() {
+    return this.isValidEmail() && this.isAge() && this.name();
+  }
 }
 
 module.exports = User;
