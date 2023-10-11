49d0ce9b6d908decc1725419493926ae51de876e
Bass913 <bastiendikiadi@outlook.fr>
2023-10-11 11:46:50 +0200

    password regex

--- user.js
+++ user.js
@@ -1,25 +1,35 @@
-const emailValidator = require('email-validator');
+const emailValidator = require("email-validator");
 
 class User {
-  constructor(firstname, lastname, email, birthdate) {
+  constructor(firstname, lastname, email, birthdate, password) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.email = email;
     this.birthdate = birthdate;
+    this.password = password;
   }
 
-
   isValid() {
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,40}$/;
     const isEmailValid = emailValidator.validate(this.email);
     const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
-    const isDateValid = dateFormatRegex.test(this.birthdate);
-    const isAgeValid =  new Date().getFullYear() - new Date(this.birthdate).getFullYear() >= 13;
+    const isValidPassword = regexPassword.test(this.password);
+    const isValidDate = dateFormatRegex.test(this.birthdate);
+    const isAgeValid =
+      new Date().getFullYear() - new Date(this.birthdate).getFullYear() >= 13;
 
     // Validation des prénoms/noms
-    const isNameValid = this.firstname.trim() !== "" && this.lastname.trim() !== "";
+    const isNameValid =
+      this.firstname.trim() !== "" && this.lastname.trim() !== "";
 
     // Retourne true si toutes les conditions sont satisfaites
-    return isEmailValid && isDateValid && isAgeValid && isNameValid;
+    return (
+      isEmailValid &&
+      isValidDate &&
+      isValidPassword &&
+      isAgeValid &&
+      isNameValid
+    );
   }
 }
 
