f879de4047809620efa9b583a92cd68b4bf9dfc7
Bass913 <bastiendikiadi@outlook.fr>
2023-10-11 11:32:22 +0200

    isValid added

--- user.js
+++ user.js
@@ -1,37 +1,39 @@
 class User {
-  constructor(firstname, lastname, email, birthdate) {
+  constructor(firstname, lastname, email, birthday, password) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.email = email;
-    this.birthdate = birthdate;
-  }
-
-  isValidEmail() {
-    // Expression régulière pour vérifier le format d'un email
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    // Teste si l'email correspond à l'expression régulière
-    return emailRegex.test(this.email);
-  }
-
-  isValidAge() {
-    const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
-    if (!dateFormatRegex.test(this.birthdate)) {
-      return false;
-    }
-
-    const today = new Date();
-    const birthDate = new Date(this.birthdate);
-
-    return today.getFullYear() - birthDate.getFullYear() >= 13;
-  }
-
-  isValidNameOrFirstName() {
-    return this.firstname !== "" && this.lastname !== "";
+    this.birthday = birthday;
+    this.password = password;
   }
 
   isValid() {
-    return this.isValidEmail() && this.isAge() && this.name();
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,40}$/;
+    return this.firstname && this.lastname && regex.test(this.email);
   }
+
+  //   isValidEmail() {
+  //     // Expression régulière pour vérifier le format d'un email
+  //     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  //     // Teste si l'email correspond à l'expression régulière
+  //     return emailRegex.test(this.email);
+  //   }
+
+  //   isValidAge() {
+  //     const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
+  //     if (!dateFormatRegex.test(this.birthday)) {
+  //       return false;
+  //     }
+
+  //     const today = new Date();
+  //     const birthday = new Date(this.birthday);
+
+  //     return today.getFullYear() - birthday.getFullYear() >= 13;
+  //   }
+
+  //   isValidNameOrFirstName() {
+  //     return this.firstname !== "" && this.lastname !== "";
+  //   }
 }
 
 module.exports = User;
