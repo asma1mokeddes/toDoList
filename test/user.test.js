9666b252a180dab4bc7cfc6593142aadd3f3f8f3
Bass913 <bastiendikiadi@outlook.fr>
2023-10-11 12:14:24 +0200

    First test good

--- test/user.test.js
+++ test/user.test.js
@@ -1,36 +1,35 @@
-const User = require('../user.js');
+const User = require("../user.js");
 var validator = require("email-validator");
 
-describe("test user email", () =>{
+describe("test user email", () => {
+  it.only("Email have the good format", () => {
+    let user = new User(
+      "Asma",
+      "MOKEDDES",
+      "asmao@pmail.com",
+      new Date().getFullYear() - 40,
+      "Mkdsdsdsds1"
+    );
+    expect(user.isValid()).toBe(true);
+  });
 
-    it.only('Email have the good format', () => {
-        let user = new User("Asma", "MOKEDDES", "asma@yopmail.com", 21);
-        expect(user.isValidEmail()).toBe(true);
+  //   it.only("Email doesnt have the good format must be true", () => {
+  //     let user = new User("", "", "asma   uyopmail.com", "");
+  //     expect(user.isValidEmail()).toBe(false);
+  //   });
 
-    })
+  //   it.only("User name is true", () => {
+  //     let user = new User("", "MOKEDDES", "", 21);
+  //     expect(user.isValidNameOrFirstName()).toBe(false);
+  //   });
 
-    it.only('Email doesnt have the good format must be true', () => {
-        let user = new User("", "", "asma   uyopmail.com", "");
-        expect(user.isValidEmail()).toBe(false);
+  //   it.only("User name is true", () => {
+  //     let user = new User("Asma", "MOKEDDES", "", 21);
+  //     expect(!user.isValidNameOrFirstName()).toBe(false);
+  //   });
 
-    })
-
-    it.only('User name is true', () => {
-        let user = new User("", "MOKEDDES", "", 21);
-        expect(user.isValidNameOrFirstName()).toBe(false);
-
-    })
-
-
-    it.only('User name is true', () => {
-        let user = new User("Asma", "MOKEDDES", "", 21);
-        expect(!user.isValidNameOrFirstName()).toBe(false);
-
-    })
-
-    it.only('AGE', () => {
-        let user = new User("Asma", "MOKEDDES", "", "2017-03-17");
-        expect(user.isValidAge()).toBe(false);
-
-    })
-})
+  //   it.only("AGE", () => {
+  //     let user = new User("Asma", "MOKEDDES", "", "2017-03-17");
+  //     expect(user.isValidAge()).toBe(false);
+  //   });
+});
--- user.js
+++ user.js
@@ -1,73 +1,27 @@
 const emailValidator = require("email-validator");
 
 class User {
-<<<<<<< HEAD
-  constructor(firstname, lastname, email, birthday, password) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.email = email;
-    this.birthday = birthday;
-=======
   constructor(firstname, lastname, email, birthdate, password) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.email = email;
     this.birthdate = birthdate;
->>>>>>> 49d0ce9b6d908decc1725419493926ae51de876e
     this.password = password;
   }
 
   isValid() {
-<<<<<<< HEAD
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,40}$/;
-    return this.firstname && this.lastname && regex.test(this.email);
-  }
-
-  //   isValidEmail() {
-  //     // Expression régulière pour vérifier le format d'un email
-  //     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  //     // Teste si l'email correspond à l'expression régulière
-  //     return emailRegex.test(this.email);
-  //   }
-
-  //   isValidAge() {
-  //     const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
-  //     if (!dateFormatRegex.test(this.birthday)) {
-  //       return false;
-  //     }
-
-  //     const today = new Date();
-  //     const birthday = new Date(this.birthday);
-
-  //     return today.getFullYear() - birthday.getFullYear() >= 13;
-  //   }
-
-  //   isValidNameOrFirstName() {
-  //     return this.firstname !== "" && this.lastname !== "";
-  //   }
-=======
     const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,40}$/;
     const isEmailValid = emailValidator.validate(this.email);
-    const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
     const isValidPassword = regexPassword.test(this.password);
-    const isValidDate = dateFormatRegex.test(this.birthdate);
-    const isAgeValid =
-      new Date().getFullYear() - new Date(this.birthdate).getFullYear() >= 13;
+    const isAgeValid = new Date().getFullYear() - this.birthdate >= 13;
 
     // Validation des prénoms/noms
     const isNameValid =
       this.firstname.trim() !== "" && this.lastname.trim() !== "";
 
     // Retourne true si toutes les conditions sont satisfaites
-    return (
-      isEmailValid &&
-      isValidDate &&
-      isValidPassword &&
-      isAgeValid &&
-      isNameValid
-    );
+    return isEmailValid && isValidPassword && isAgeValid && isNameValid;
   }
->>>>>>> 49d0ce9b6d908decc1725419493926ae51de876e
 }
 
 module.exports = User;
