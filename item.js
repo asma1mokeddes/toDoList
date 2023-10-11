01af30096ec440164ab7fd7d53816ea972b50e92
ataslima <ataslima@linguaphone.fr>
2023-10-11 12:07:22 +0200

    class item and toDo

---
+++ item.js
@@ -0,0 +1,11 @@
+class  Item{
+
+  constructor(name, content, creationDate) {
+    this.name = name;
+    this.content = content;
+    this.creationDate = creationDate;
+  }
+  
+}
+
+module.exports = Item;
--- toDo.js
+++ toDo.js
@@ -1,16 +1,27 @@
 class  ToDo{
 
-
-    constructor(){
-        
+    constructor() {
+        this.items = [];
     }
-
-
-    add() {
-        
-    }
-
     
+    canAddItem() {
+        if (this.items.length >= 10) {
+            return false;
+        }
+        const isUniqueName = this.items.every(existingItem => existingItem.name !== item.name);
+
+        const isContentValid = item.content.length <= 1000;
+
+        return isUniqueName && isContentValid;
+    }
+
+    add(item) {
+        if (this.canAddItem()) {
+            this.items.push(item);
+        } else {
+            throw new Error("Cannot add item");
+        }
+    }  
 }
 
 module.exports = ToDo;
