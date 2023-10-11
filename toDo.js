8b040a3e1c757facc0a2afeeacd7f41b4bba78a9
ataslima <ataslima@linguaphone.fr>
2023-10-11 14:15:34 +0200

    toDo test

--- toDo.js
+++ toDo.js
@@ -1,23 +1,49 @@
+const Item = require("./item.js");
+
 class  ToDo{
+
+   
 
     constructor() {
         this.items = [];
     }
+
+    verifyItemType(item) {
+        if( item instanceof Item){
+
+            return true
+        } 
+        return false
+    }
+
     
-    canAddItem() {
-        if (this.items.length >= 10) {
+    canAddItem(item) {
+        if (this.items.length >= 10 || !this.verifyItemType(item)) {
             return false;
         }
+        
         const isUniqueName = this.items.every(existingItem => existingItem.name !== item.name);
 
         const isContentValid = item.content.length <= 1000;
+
 
         return isUniqueName && isContentValid;
     }
 
     add(item) {
-        if (this.canAddItem()) {
-            this.items.push(item);
+        if (this.canAddItem(item)) {
+            if (this.items.length === 0) {
+                this.items.push(item);
+            } else {
+                const lastItem = this.items[this.items.length - 1];
+                const timeDifference = item.creationDate - lastItem.creationDate;
+          
+                if (timeDifference >= 30 * 60 * 1000) {
+                  this.items.push(item);
+                } else {
+                    throw new Error("The 30 minute period was not respected.");
+                }
+            }
         } else {
             throw new Error("Cannot add item");
         }
