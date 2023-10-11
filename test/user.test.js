7ac5aece5c5eb39558c4a1943f1e682c98f50f77
asma1mokeddes <asma1>
2023-10-11 12:32:41 +0200

    fix(tests): fix user tests

--- test/user.test.js
+++ test/user.test.js
@@ -2,34 +2,58 @@ const User = require("../user.js");
 var validator = require("email-validator");
 
 describe("test valid user", () => {
-  it.only("Test if a user is valid", () => {
-    let user = new User(
-      "Asma",
-      "MOKEDDES",
-      "asmao@pmail.com",
-      new Date().getFullYear() - 40,
-      "Mkdsdsdsds1"
-    );
-    expect(user.isValid()).toBe(true);
-  });
+    it("Test if a user is valid with valid input", () => {
+        let user = new User(
+            "John",
+            "Doe",
+            "johndoe@pmail.com",
+            new Date().getFullYear() - 40,
+            "Mkdsdsdsds1"
+        );
+        expect(user.isValid()).toBe(true);
+    });
 
-  //   it.only("Email doesnt have the good format must be true", () => {
-  //     let user = new User("", "", "asma   uyopmail.com", "");
-  //     expect(user.isValidEmail()).toBe(false);
-  //   });
+    it("Test if a user is valid with an invalid email", () => {
+        let user = new User(
+            "Alice",
+            "Smith",
+            "invalid-email",
+            new Date().getFullYear() - 70,
+            "Mkdsdsdsds1"
+        );
+        expect(user.isValid()).toBe(false);
+    });
 
-  //   it.only("User name is true", () => {
-  //     let user = new User("", "MOKEDDES", "", 21);
-  //     expect(user.isValidNameOrFirstName()).toBe(false);
-  //   });
+    it("Test if a user is valid with a birthdate indicating under 13 years old", () => {
+        let user = new User(
+            "Eva",
+            "Adams",
+            "eva@example.com",
+            new Date().getFullYear() - 5,
+            "StrongPassword123"
+        );
+        expect(user.isValid()).toBe(false);
+    });
 
-  //   it.only("User name is true", () => {
-  //     let user = new User("Asma", "MOKEDDES", "", 21);
-  //     expect(!user.isValidNameOrFirstName()).toBe(false);
-  //   });
+    it("Test if a user is valid with empty first name and last name", () => {
+        let user = new User(
+            "",
+            "",
+            "empty@example.com",
+            new Date().getFullYear() - 30,
+            "ComplexPassword123"
+        );
+        expect(user.isValid()).toBe(false);
+    });
 
-  //   it.only("AGE", () => {
-  //     let user = new User("Asma", "MOKEDDES", "", "2017-03-17");
-  //     expect(user.isValidAge()).toBe(false);
-  //   });
+    it("Test if a user is valid with a weak password", () => {
+        let user = new User(
+            "Bob",
+            "Johnson",
+            "bob@example.com",
+            new Date().getFullYear() - 60,
+            "weak"
+        );
+        expect(user.isValid()).toBe(false);
+    });
 });
